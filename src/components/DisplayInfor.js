cd08b12ad1fd064132b14f9f67d16285f0a53059
Alfie Tran <alfie.tran245@gmail.com>
2025-07-17 16:06:05 +0700

    life cycle Reactjs-class

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -5,15 +5,38 @@ import logo from './../logo.svg';
 //props => viết tắt của properties: tài sản
 
 class DisplayInfor extends React.Component {
-	state = {
-		isShowListUsers: true,
-	};
+	constructor(props) {
+		console.log('>>>call constructor: 1');
+		super(props);
+		//babel compiler
+		this.state = {
+			isShowListUsers: true,
+		};
+	}
+
+	componentDidMount() {
+		console.log('>>>call me component did mount');
+		setTimeout(() => {
+			document.title = 'AlDevX vs Hoi Dan IT';
+		}, 3000);
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		console.log('>>>call me component did update', this.props, prevProps);
+		if (this.props.listUsers !== prevProps.listUsers) {
+			if (this.props.listUsers.length === 5) {
+				alert('You got 5 users');
+			}
+		}
+	}
+
 	handleShowHide = () => {
 		this.setState({
 			isShowListUsers: !this.state.isShowListUsers,
 		});
 	};
 	render() {
+		console.log('>>>call me render');
 		//destructuring array/object
 		const { listUsers } = this.props;
 		//console.log(listUsers); // console.table(listUsers);
