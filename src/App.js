6b6a3548db9d84203ae4369b46b8f596e67cbfb7
Alfie Tran <alfie.tran245@gmail.com>
2026-09-09 08:34:21 +0700

    thay doi the Nav.Link ==> <Link to='/'>  de ko reload lai trang

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
-
+import { Link } from 'react-router-dom'; //thu vien dieu huong nguoi dung
 const App = () => {
 	return (
 		<div className="App">
 			<Header />
+			<div>
+				test link
+				<div>
+					<button>
+						{/* Link to: dieu huong nguoi dung. Ben trong thu vien da co ham event.preventDefault cho nen no ko can reload lai chuong trinh*/}
+						<Link to="/users">Go to User page</Link>
+					</button>
+					<button>
+						<Link to="/admins">Go to Admin page</Link>
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/components/User/User.js
@@ -0,0 +1,5 @@
+const User = (props) => {
+	return <div>user component</div>;
+};
+
+export default User; //de use dc component nay trong index.js
