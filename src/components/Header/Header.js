fdfc604c2f5072115ff06b5e3a3d62624a7a9c04
Alfie Tran <alfie.tran245@gmail.com>
2026-09-09 09:31:42 +0700

    NavLink de active link doi mau cho user biet dang o trang nao va scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom'; //thu vien dieu huong nguoi dung
+import { NavLink } from 'react-router-dom'; //Link: thu vien dieu huong nguoi dung
 const Header = () => {
 	return (
 		<Navbar expand="lg" className="bg-body-tertiary">
@@ -11,15 +11,15 @@ const Header = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Link to="/" className="nav-link">
+						<NavLink to="/" className="nav-link">
 							Home
-						</Link>
-						<Link to="/users" className="nav-link">
+						</NavLink>
+						<NavLink to="/users" className="nav-link">
 							Users
-						</Link>
-						<Link to="/admins" className="nav-link">
+						</NavLink>
+						<NavLink to="/admins" className="nav-link">
 							Admin
-						</Link>
+						</NavLink>
 					</Nav>
 					<Nav>
 						<NavDropdown title="Settings" id="basic-nav-dropdown">
