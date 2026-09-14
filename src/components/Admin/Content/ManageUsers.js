94db3747940313e202a823b63f6cb88db75ba8d4
Alfie Tran <alfie.tran245@gmail.com>
2026-09-14 10:59:28 +0700

    dung modal va handle upload file thong qua state cua react; css lai

--- src/components/Admin/Content/ManageUsers.js
+++ src/components/Admin/Content/ManageUsers.js
@@ -1,16 +1,16 @@
 import ModalCreateUser from './ModalCreateUser';
+import './ManageUsers.scss';
 const ManageUsers = (props) => {
 	return (
 		<div className="manage-user-container">
 			<div className="title">Manage Users</div>
-			<div className="manage-content">
+			<div className="users-content">
 				<div className="">
 					<button>Add new Users</button>
 				</div>
-				<div className="">
-					table user
-					<ModalCreateUser />
-				</div>
+				<div className="">table user</div>
+
+				<ModalCreateUser />
 			</div>
 		</div>
 	);
